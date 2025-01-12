a1e73dd608e3aa95c2d90a8069a17805f2bbe255
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2025-01-12 08:04:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,24 +9,26 @@ from pyrogram import filters
 load_dotenv()
 # ------------------------------------
 # -----------------------------------------------------
-API_ID = int(getenv("25725544"))
-API_HASH = getenv("d174c017101e48ecaa6a3a76a221c878")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+EVAL = list(map(int, getenv("EVAL", "7427691214 7045191057").split()))
 # ------------------------------------------------------
-BOT_TOKEN = getenv("7566302231:AAE6uuVx48aBw0ffP1aREiZC6--3adM1_mk")
+BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","OG_LORD_PSYCHO")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Ownergit")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "OREX_PROBOT")
+BOT_USERNAME = getenv("BOT_USERNAME" , "NexikoBot")
 # --------------------------------------------------------
-BOT_NAME = getenv("BOT_NAME" , "𝐎ʀᴇx ꭙ 𝐌ᴜsɪᴄ")
+BOT_NAME = getenv("BOT_NAME" , "Nexiko")
 # ---------------------------------------------------------
-ASSUSERNAME = getenv("ASSUSERNAME" , "OREX_ASSISTANT")
+ASSUSERNAME = getenv("ASSUSERNAME" , "MissYumikoo")
 # ---------------------------------------------------------
 
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://anandcollage245:ZehHlJTPgnFr8AjH@cluster0.t7w7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0)
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
@@ -35,10 +37,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", -1002365374424))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002237336934))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 7462077192))
+OWNER_ID = int(getenv("OWNER_ID", 7427691214))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -53,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/DAXXTEAM/DAXXMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
@@ -67,8 +69,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-CHAT_GROUP= getenv("CHAT_GROUP", "https://t.me/THE_SOULMATES_HUB")
-OWNER_CHANNEL= getenv("SUPPORT_CHAT", "https://t.me/OG_PSYCHO_XD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ALLTYPECC")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/gitwizardbypass")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -141,21 +143,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://i.ibb.co/1K7xVRy/photo-2024-08-09-12-50-59-7457678412732170244.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://i.ibb.co/cF3PDZB/photo-2024-08-09-12-51-02-7457678520106352676.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
 )
-PLAYLIST_IMG_URL = "https://i.ibb.co/d4xNW3f/photo-2024-08-09-12-50-46-7457679417754517508.jpg"
-STATS_IMG_URL = "https://i.ibb.co/qxSZXpb/photo-2024-10-14-20-18-57-7457679503653863432.jpg"
-TELEGRAM_AUDIO_URL = "https://i.ibb.co/1qYF4DZ/photo-2024-10-14-20-18-58-7457679933150593040.jpg"
-TELEGRAM_VIDEO_URL = "https://i.ibb.co/1qYF4DZ/photo-2024-10-14-20-18-58-7457679933150593040.jpg"
-STREAM_IMG_URL = "https://i.ibb.co/d4xNW3f/photo-2024-08-09-12-50-46-7457679417754517508.jpg"
-SOUNCLOUD_IMG_URL = "https://i.ibb.co/qxSZXpb/photo-2024-10-14-20-18-57-7457679503653863432.jpg"
-YOUTUBE_IMG_URL = "https://i.ibb.co/d4xNW3f/photo-2024-08-09-12-50-46-7457679417754517508.jpg"
-SPOTIFY_ARTIST_IMG_ = "https://i.ibb.co/1qYF4DZ/photo-2024-10-14-20-18-58-7457679933150593040.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/d4xNW3f/photo-2024-08-09-12-50-46-7457679417754517508.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/1qYF4DZ/photo-2024-10-14-20-18-58-7457679933150593040.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
